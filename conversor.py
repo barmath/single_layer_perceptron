e0a58fc649da4edb3a5c33ffe77b3cbe96be1250
bar_math <matheus_barbosa@usp.br>
2021-05-21 11:28:51 -0300

    data_organize ready

--- conversor.py
+++ conversor.py
@@ -1,14 +1,17 @@
 from reader import CSV_reader 
 
-training_data = CSV_reader.read_csv('caracteres-limpo2.csv')
+def data_organizer(name_of_file):
+	training_data = CSV_reader.read_csv(name_of_file)
 
-training_inputs = [ [] for _ in range(len(training_data))]
-labels = [ [] for i in range(len(training_data))]
+	training_inputs = [ [] for _ in range(len(training_data))]
+	labels = [ [] for i in range(len(training_data))]
 
-for i in range(len(training_data)):
-	training_inputs[i] = training_data[i][0:-8] 
-	labels[i] = training_data[i][-7:-1]
+	for i in range(len(training_data)):
+		training_inputs[i] = training_data[i][0:-8] 
+		labels[i] = training_data[i][-7:-1]
 
-print(training_inputs)
-print(labels)
-print(len(training_data))
+	print(training_inputs)
+	print(labels)
+	print(len(training_data))
+
+data_organizer('caracteres-limpo2.csv')
--- usage2.py
+++ usage2.py
@@ -25,22 +25,25 @@ def conversor(name_of_file):
 	return training_inputs, labels
 
 def data_organizer(name_of_file):
-	training_data2 = CSV_reader.read_csv(name_of_file)
+	training_data = CSV_reader.read_csv(name_of_file)
 
-	training_inputs2 = [ [] for _ in range(len(training_data2))]
-	labels2 = [ [] for i in range(len(training_data2))]
+	training_inputs = [ [] for _ in range(len(training_data))]
+	labels = [ [] for i in range(len(training_data))]
 
 	for i in range(len(training_data)):
-		training_inputs2[i] = training_data2[i][0:-8] 
-		labels2[i] = training_data2[i][-7:-1]
-	
-	print(training_data2)
-	print(labels2)
+		training_inputs[i] = training_data[i][0:-8] 
+		labels[i] = training_data[i][-7:-1]
 
-training_inputs, labels = conversor('td_lorry.csv')
+	training_inputs = [ [ int(training_inputs[i][j]) for j in range(len(training_inputs[0])) ] for i in range(len(training_inputs)) ]
+	labels = [ [ int(labels[i][j]) for j in range(len(labels[0])) ] for i in range(len(labels)) ]
+	return training_inputs, labels
+
+training_inputs, labels =conversor('td_lorry.csv')
+#training_inputs, labels = data_organizer('caracteres-limpo2.csv')
 
 print(training_inputs)
 print(labels)
+
 
 #data_organizer('caracteres-limpo2.csv')
 # Trainning step  
@@ -49,7 +52,7 @@ perceptron = Perceptron(2)
 perceptron.train(training_inputs, labels)
 
 # Test of the prediction 
-
+'''
 inputs = [0,0]
 result =  []
 
@@ -60,7 +63,7 @@ for i in range(1,len(prediction_tests)):
 	inputs[1] = int(prediction_tests[i][1])
 	result = perceptron.predict(inputs)
 	print(f'The result for {prediction_tests[i][0]}, {prediction_tests[i][1]} = {result} the true is {prediction_tests[i][2]}.')
-
+'''
 
 
 
